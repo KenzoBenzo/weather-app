43887371628dc0c21f6d4f2e74eca3161af30468
Makenna Smutz <makennasmutz@gmail.com>
2020-07-07 22:06:45 +0200

    sidebar design
    Co-Authored-By: miharuza <66176706+miharuza@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,14 @@ import {
   Image,
   Heading,
   Text,
+  Stack,
+  Icon,
 } from "@chakra-ui/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import shower from "../images/Shower.png"
+import Background from "../images/Cloud-Background.png"
+import BackgroundLight from "../images/Cloud-Background-Light.png"
 
 const IndexPage = () => {
   const { colorMode, toggleColorMode } = useColorMode()
@@ -23,25 +27,64 @@ const IndexPage = () => {
         <Box
           backgroundColor={colorMode === "light" ? "gray.100" : "gray.800"}
           w="25%"
+          padding={8}
         >
-          <nav>
-            <Button variantColor="gray">Search for places</Button>
+          <Flex justifyContent="space-between">
+            <Button variantColor="blue">Search for places</Button>
             <IconButton
-              variantColor="gray"
+              isRound
+              variantColor="blue"
               aria-label="Search database"
               icon="compass"
             />
-          </nav>
-          <Box>
-            <Image src={shower} />
+          </Flex>
+          <Box
+            backgroundImage={`url(${
+              colorMode === "light" ? BackgroundLight : Background
+            })`}
+            bgSize="cover"
+            mr={-8}
+            ml={-8}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            mt={8}
+            py={8}
+          >
+            <Image w="200px" h="auto" src={shower} />
           </Box>
-          <Heading as="h1">15℃</Heading>
-          <Heading as="h2">Shower</Heading>
-          <Flex>
+          <Flex justifyContent="center" alignItems="baseline">
+            <Heading as="h1" fontSize="144px" fontWeight={500}>
+              15
+            </Heading>
+            <Text fontSize="5xl" color="gray.500">
+              ℃
+            </Text>
+          </Flex>
+          <Heading
+            textAlign="center"
+            mt={8}
+            color="gray.500"
+            fontWeight={600}
+            as="h2"
+          >
+            Shower
+          </Heading>
+          <Stack isInline justifyContent="center" mt={16} color="gray.500">
             <Text>Today</Text>
             <Text>•</Text>
             <Text>Fri, 5 Jun</Text>
-          </Flex>
+          </Stack>
+          <Stack
+            isInline
+            justifyContent="center"
+            mt={4}
+            color="gray.500"
+            alignItems="center"
+          >
+            <Icon name="pin" />
+            <Text>Helsinki</Text>
+          </Stack>
         </Box>
 
         <Box
--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ import { theme } from "@chakra-ui/core"
 
 const customTheme = {
   ...theme,
+  fonts: {
+    heading: `"Raleway", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    body: `"Raleway", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  },
   icons: {
     ...theme.icons,
     compass: {
@@ -13,6 +17,15 @@ const customTheme = {
             fill="currentColor"
           />
         </g>
+      ),
+      viewBox: "0 0 24 24",
+    },
+    pin: {
+      path: (
+        <path
+          d="M12 2C8.13 2 5 5.13 5 9C5 13.17 9.42 18.92 11.24 21.11C11.64 21.59 12.37 21.59 12.77 21.11C14.58 18.92 19 13.17 19 9C19 5.13 15.87 2 12 2ZM12 11.5C10.62 11.5 9.5 10.38 9.5 9C9.5 7.62 10.62 6.5 12 6.5C13.38 6.5 14.5 7.62 14.5 9C14.5 10.38 13.38 11.5 12 11.5Z"
+          fill="currentColor"
+        />
       ),
       viewBox: "0 0 24 24",
     },
